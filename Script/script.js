392f0a85468e0f25bc55c13a7d61f1d91c8dc875
fardin-rahman-sojon <sojoneditz25@gmail.com>
2025-09-05 12:31:44 +0600

    updata modal section

--- Script/script.js
+++ Script/script.js
@@ -1,4 +1,19 @@
-console.log('I am JS');
+
+const createElements = (arr) => {
+    const htmlElements = arr.map(el => `<span class="btn bg-[#1a91ff1a]">${el}</span>`)
+    return htmlElements.join(" ");
+}
+
+const manageSpiner = (stutus)=>{
+    if(stutus == true){
+        document.getElementById("spinner").classList.remove("hidden");
+        document.getElementById("word-container").classList.add("hidden");
+    }
+    else{
+        document.getElementById("word-container").classList.remove("hidden");
+        document.getElementById("spinner").classList.add("hidden");
+    }
+}
 
 const LoadLessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all") //Promise of response
@@ -6,24 +21,72 @@ const LoadLessons = () => {
         .then((json) => displayLesson(json.data));
 };
 
-const removeActive = ()=> {
+const removeActive = () => {
     const lessonButtons = document.querySelectorAll(".lesson-btn")
     // console.log(lessonButtons);
     lessonButtons.forEach((btn) => btn.classList.remove("active"))
-      
+
 }
 
 const loadLevalWord = (id) => {
+    manageSpiner(true);
     const url = `https://openapi.programming-hero.com/api/level/${id}`
     fetch(url)
         .then((res) => res.json())
         .then((data) => {
             removeActive(); // remove all active class
-            const clickBtn =document.getElementById(`lesson-btn-${id}`)
+            const clickBtn = document.getElementById(`lesson-btn-${id}`)
             // console.log(clickBtn);
             clickBtn.classList.add("active"); // add avtive class
             displayLevalWord(data.data)
         })
+}
+
+const loadWordDetail = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/word/${id}`;
+    // console.log(url);
+    const res = await fetch(url);
+    const details = await res.json();
+    displayWordDetails(details.data)
+}
+
+// {
+//     "word": "Cautious",
+//     "meaning": "সতর্ক",
+//     "pronunciation": "কশাস",
+//     "level": 2,
+//     "sentence": "Be cautious while crossing the road.",
+//     "points": 2,
+//     "partsOfSpeech": "adjective",
+//     "synonyms": [
+//         "careful",
+//         "alert",
+//         "watchful"
+//     ],
+//     "id": 3
+// }
+
+const displayWordDetails = (word) => {
+    console.log(word)
+    const detailsBox = document.getElementById("details-container");
+    detailsBox.innerHTML = `
+    <div class="">
+        <h2 class="text-2xl">${word.word} ( <i class="fa-solid fa-microphone-lines"></i> :${word.pronunciation})</h2>
+      </div>
+      <div class="">
+        <h2 class="font-bold">Meaning</h2>
+        <p>${word.meaning}</p>
+      </div>
+      <div class="">
+        <h2 class="font-bold">Example</h2>
+        <p>${word.sentence}</p>
+      </div>
+        <div class="">
+        <h2 class="font-bold mb-1">সমার্থক শব্দ গুলো</h2>
+        <div class="">${createElements(word.synonyms)}</div>
+    </div>
+    `
+    document.getElementById("word_modal").showModal();
 }
 
 const displayLevalWord = (words) => {
@@ -38,6 +101,7 @@ const displayLevalWord = (words) => {
             <h3 class="font-bold text-2xl">নেক্সট Lesson এ যান</h3>
           </div>
         `;
+        manageSpiner(false);
         return;
     }
 
@@ -57,14 +121,16 @@ const displayLevalWord = (words) => {
             <h2 class="text-xl font-bold">${word.word ? word.word : "শব্দ পাওয়া যাইনি"}</h2>
             <p class="font-medium">${word.meaning ? word.meaning : "অর্থ পাওয়া যাইনি"}</p>
             <div class="hind-siliguri-font text-2xl font-medium">${word.pronunciation ? word.pronunciation : "Pronounciation পাওয়া যাইনি"}</div>
+
             <div class="flex justify-between items-center">
-              <button onclick="my_modal_5.showModal()" class="btn bg-[#1a91ff1a] hover:bg-[#1a91ff80] rounded-lg p-2"><i class="fa-solid fa-circle-info"></i></button>
+              <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1a91ff1a] hover:bg-[#1a91ff80] rounded-lg p-2"><i class="fa-solid fa-circle-info"></i></button>
               <button class="btn bg-[#1a91ff1a] hover:bg-[#1a91ff80] rounded-lg p-2"><i class="fa-solid fa-volume-high"></i></button>
             </div>
           </div>
         `
         wordContainer.append(card)
     });
+    manageSpiner(false);
 }
 
 const displayLesson = (lessons) => {
